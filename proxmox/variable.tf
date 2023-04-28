d6e02a36c9a2c11394742c8475b1d2a7c766b711
Denys Rassokha <rosden92@gmail.com>
2023-04-28 19:37:22 +0300

    add tf proxmox

---
+++ proxmox/variable.tf
@@ -0,0 +1,18 @@
+variable "url" {
+default = "https://192.168.31.200:8006/api2/json"
+}
+variable "token_id" {
+  default = "root@pam!root"
+}
+variable "token_secret" {
+  default = "d638b884-1a83-47b6-b6fd-de36fe52af73"
+}
+variable "server" {
+  default = 4
+}
+variable "node" {
+  default = "google"
+}
+variable "vm_teplate" {
+  default = "ubuntu"
+}
